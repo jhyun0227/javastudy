a3c4fce4fec1aff61798fb9bb2c7b4f00c8834c9
준호 <83191821+jhyun0227@users.noreply.github.com>
2023-01-15 21:52:08 +0900

    이것이 자바다 멀티쓰레드 부분 공부

--- src/main/java/practice/javastudy/ch12/threadStatus/TargetThread.java
+++ src/main/java/practice/javastudy/ch12/threadStatus/TargetThread.java
@@ -3,13 +3,21 @@ package practice.javastudy.ch12.threadStatus;
 public class TargetThread extends Thread {
     @Override
     public void run() {
-        for (long i = 0; i < 100000000; i++) {}
+        for (long i = 0; i < 100000000; i++) {
+            if (i == 50000000) {
+                System.out.println("===");
+            }
+        }
 
         try {
             //1.5초간 일시정지
             Thread.sleep(1500);
         } catch (Exception e) {}
 
-        for (long i = 0; i < 100000000; i++) {}
+        for (long i = 0; i < 100000000; i++) {
+            if (i == 50000000) {
+                System.out.println("===");
+            }
+        }
     }
 }
